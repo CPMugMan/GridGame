a278e059bd4c7097f826900a264f38706dbd2164
CPMugMan <nhatdao97@gmail.com>
2018-10-02 10:35:27 +0800

    Initial Commit

---
+++ app/src/main/java/au/edu/curtin/gridgame/OpeningScreenActivity.java
@@ -0,0 +1,15 @@
+package au.edu.curtin.gridgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningScreenActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+    }
+}
